3740cc2de0b74c24d532ac4d9722d199bf81d6c7
Debashis Roy Bhowmik <debashis.buet08@gmail.com>
2019-07-28 12:09:35 +0600

    rename prefixer and uglify added and browsersync watch configured

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,46 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const auto_prefixer = require('gulp-autoprefixer');
+const uglify_css = require('gulp-uglifycss');
 const browser_sync = require('browser-sync').create();
 
 
 function style() {
-    // 1. scss file location
+    // scss file location
     return gulp.src('./src/scss/*.scss')
 
-    // 2. pass those file to sass compiler
+    // pass those file to sass compiler
         .pipe(sass())
+
+    // css prefix for browsers compatibility (browsers listed on package.json)
+        .pipe(auto_prefixer())
     
-    // 3. compiled css file location
+    // minify css file
+		.pipe(uglify_css())
+
+    // rename output file
+        .pipe(rename({suffix: '.min'}))
+    
+    // set compiled css file location
         .pipe(gulp.dest('./build/css'))
+
+    // stream changes to browser
+        .pipe(browser_sync.stream());
+}
+
+function watch() {
+    browser_sync.init({
+        server: {
+            baseDir: './build'
+        }
+    });
+
+    // watch for these file changes
+    gulp.watch('./src/scss/**/*.scss', style);
+    gulp.watch('./build/*.html').on('change', browser_sync.reload);
+    gulp.watch('./build/*.js').on('change', browser_sync.reload);
 }
 
 exports.style = style;
+exports.watch = watch;
